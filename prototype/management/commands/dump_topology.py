d3e27c4a148eb653cf18ffd8d5bbd2a4c1399a5f
Ben Thomasson <bthomass@ansible.com>
2017-03-30 10:03:15 -0400

    Extract yaml_serialize_topology into prototype.serializers

--- prototype/management/commands/dump_topology.py
+++ prototype/management/commands/dump_topology.py
@@ -1,18 +1,6 @@
 from django.core.management.base import BaseCommand
-from prototype.models import Topology, Device, Link, Interface
-from django.db.models import Q
 
-import yaml
-
-
-NetworkAnnotatedInterface = Interface.objects.values('name',
-                                                     'from_link__pk',
-                                                     'to_link__pk',
-                                                     'from_link__to_device__name',
-                                                     'to_link__from_device__name',
-                                                     'from_link__to_interface__name',
-                                                     'to_link__from_interface__name')
-
+from prototype.serializers import yaml_serialize_topology
 
 
 
@@ -25,35 +13,4 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         topology_id = options['topology_id']
 
-        data = dict(devices=[],
-                    links=[])
-
-        topology = Topology.objects.get(pk=topology_id)
-
-        data['name'] = topology.name
-
-        links = list(Link.objects
-                         .filter(Q(from_device__topology_id=topology_id) |
-                                 Q(to_device__topology_id=topology_id)))
-
-        interfaces = Interface.objects.filter(device__topology_id=topology_id)
-
-        for device in Device.objects.filter(topology_id=topology_id).order_by('name'):
-            interfaces = list(NetworkAnnotatedInterface.filter(device_id=device.pk).order_by('name'))
-            interfaces = [dict(name=x['name'],
-                               network=x['from_link__pk'] or x['to_link__pk'],
-                               remote_device_name=x['from_link__to_device__name'] or x['to_link__from_device__name'],
-                               remote_interface_name=x['from_link__to_interface__name'] or x['to_link__from_interface__name'],
-                              ) for x in interfaces]
-            data['devices'].append(dict(name=device.name,
-                                        type=device.type,
-                                        interfaces=interfaces))
-
-        for link in links:
-            data['links'].append(dict(from_device=link.from_device.name,
-                                      to_device=link.to_device.name,
-                                      from_interface=link.from_interface.name,
-                                      to_interface=link.to_interface.name,
-                                      network=link.pk))
-
-        print yaml.safe_dump(data, default_flow_style=False)
+        print yaml_serialize_topology(topology_id)
